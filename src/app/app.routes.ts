477ffbab150ab97d0a363e89dd07aa87d1890e06
DerBlockProgrammer <marcvossler@outlook.de>
2025-01-02 10:56:50 +0100

    d

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { MainComponent } from './mainPage/mainPage.component';
+import { ImpressumComponent } from './mainPage/impressum/impressum.component';
+
 
 export const routes: Routes = [
-    {path:'', component: MainComponent}
+    {path:'', component: MainComponent},
+    { path: 'impressum', component: ImpressumComponent },
+    { path: '**', redirectTo: '' } // Für ungültige Routen
 ];
